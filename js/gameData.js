373b1679c91a3a2e27f01dd59539c48d11ab2a93
Radoslaw Jakubiak <radoslaw.jakubiak@experienceone.com>
2020-01-30 13:15:47 +0100

    MInor changes

---
+++ js/gameData.js
@@ -0,0 +1,47 @@
+var characterClassesData = {
+    warrior: {
+        names: ["Jurand", "Zawisza", "Siegfried", "Ironskin", "Harald", "Marco", "Francis"],
+        stats: {
+            strength: 14,
+            agility: 8,
+            dexterity: 9,
+            vitality: 13,
+            intelligence: 7,
+            willpower: 9
+        }
+    },
+    ranger: {
+        names: ["Wilhelm", "Robin", "Jean", "Baldwin", "Vasyl", "Tanja", "Xiu"],
+        stats: {
+            strength: 7,
+            agility: 8,
+            dexterity: 16,
+            vitality: 7,
+            intelligence: 9,
+            willpower: 4
+        }
+    },
+    cleric: {
+        names: ["Avicenna", "Hyppokrates", "Guldorf", "Coronavix", "Thomas", "Marcel", "Mia"],
+        stats: {
+            strength: 5,
+            agility: 8,
+            dexterity: 7,
+            vitality: 10,
+            intelligence: 13,
+            willpower: 14
+        }
+    },
+    mage: {
+        names: ["Merlin", "Strange", "Alucard", "Houdini", "Maryanna", "Victoria", "Weldermort"]
+    },
+    stats: {
+        strength: 5,
+        agility: 8,
+        dexterity: 8,
+        vitality: 5,
+        intelligence: 19,
+        willpower: 14
+    }
+}
+
